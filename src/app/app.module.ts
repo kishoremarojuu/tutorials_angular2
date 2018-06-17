0e99b3f5f14c7997a42ee95dbb05bcbd45289b61
kishoremarojuu <kishoremarojuu>
2018-06-16 19:22:38 -0500

    addingProviderClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component'; //import { AppComponent } from './springboot.component';
+import { AppComponent } from './app.component'; 
 import { HelloComponent } from './hello.component';
-//import { EmployeeComponent} from './employee/employee.component';
+//import { SpringBootcomponent } from './springboot.component';
+import { EmployeeComponent} from './employee/employee.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,EmployeeComponent],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+  
+ }
--- src/app/springboot.ts
+++ src/app/springboot.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class SpringBootcomponent  {
   name = 'Welcome to Spring Boot';
 }
